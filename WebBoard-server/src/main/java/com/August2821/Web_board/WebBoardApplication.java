0381587d268817a61b921fd3d2593c9e386a9bd0
August2821 <gosohanjinnunkkaebi@gmail.com>
2025-08-05 21:27:05 +0900

    spring boot

---
+++ WebBoard-server/src/main/java/com/August2821/Web_board/WebBoardApplication.java
@@ -0,0 +1,13 @@
+package com.August2821.Web_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBoardApplication.class, args);
+	}
+
+}
